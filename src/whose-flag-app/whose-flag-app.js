2f66f6c79b89852e6d9b19af55384d367e9f7e90
John Riviello <John_Riviello@Comcast.com>
2018-05-30 16:05:10 -0400

    End of Step 6. Style your app

--- src/whose-flag-app/whose-flag-app.js
+++ src/whose-flag-app/whose-flag-app.js
@@ -2,6 +2,7 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/app-layout/app-layout.js';
 import '@polymer/iron-image/iron-image.js';
 import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-styles/color.js';
 
 /**
  * @customElement
@@ -13,6 +14,45 @@ class WhoseFlagApp extends PolymerElement {
       <style>
         :host {
           display: block;
+          font-family: Roboto, Noto, sans-serif;
+        }
+        paper-button {
+          color: white;
+        }
+        paper-button.another {
+          background: var(--paper-blue-500);
+          width: 100%;
+        }
+        paper-button.another:hover {
+          background: var(--paper-light-blue-500);
+        }
+        paper-button.answer {
+          background: var(--paper-purple-500);
+          flex-grow: 1;
+        }
+        paper-button.answer:hover {
+          background: var(--paper-pink-500);
+        }
+        app-toolbar {
+          background-color: var(--paper-blue-500);
+          color: white;
+          margin: 20px 0;
+        }
+        iron-image {
+          border: solid;
+          width: 100%;
+          --iron-image-width: 100%;
+          background-color: white;
+        }
+        #flag-image-container {
+          max-width: 600px;
+          width: 100%;
+          margin: 0 auto;
+        }
+        #answer-button-container {
+          display: flex; /* or inline-flex */
+          flex-flow: row wrap;
+          justify-content:space-around;
         }
       </style>
       <app-header>
